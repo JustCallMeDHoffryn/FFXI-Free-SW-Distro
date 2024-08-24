c3af37d96a490ffc55f42ed5675565a1950bfdbd
D'Hoffryn <64779726+JustCallMeDHoffryn@users.noreply.github.com>
2024-08-24 11:05:15 +0200

    Version 0.9 alpha
    Version 0.9 alpha

--- TickerTape/data/RulesIn.lua
+++ TickerTape/data/RulesIn.lua
@@ -2,18 +2,36 @@ local AllRulesIn = {
 
 --	NPC Update
 	
-	[0x00E] = T	{			--		Offset	bit		Name		format		decode		Info
-					[1]		=	{	0,		0,	 	'ID',		'byte',		'raw',		''},
-					[2]		=	{	1,		0,	 	'Size',		'byte',		'raw',		''},
-					[3]		=	{	2,		0,	 	'Sync',		'rword',	'raw',		''},
-					[4]		=	{	4,		0,	 	'NPC',		'rdword',	'entity',	''},
-					[5]		=	{	8,		0,	 	'Index',	'rword',	'raw',		''},
-					[6]		=	{	10,		1,	 	'Flag',		'special',	'bool',		'[Position, Rotation, Walk]'	},
-					[7]		=	{	12,		0,	 	'Position',	'special',	'xyz',		''},
-					[8]		=	{	10,		2,	 	'Flag',		'special',	'bool',		'[Claimer ID]'	},
-					[9]		=	{	10,		4,	 	'Flag',		'special',	'bool',		'[HP, Status]'	},
-					[10]	=	{	10,		8,	 	'Flag',		'special',	'bool',		'[Name]'	},
+	[0x00E] = T	{			--		Offset	bit		Name		format		logic		flag		decode		Info
+					[1]		=	{	0,		0,	 	'ID',		'byte',		'none',		0,			'raw',		''},
+					[2]		=	{	1,		0,	 	'Size',		'byte',		'none',		0,			'raw',		''},
+					[3]		=	{	2,		0,	 	'Sync',		'rword',	'none',		0,			'raw',		''},
+					[4]		=	{	4,		0,	 	'NPC',		'rdword',	'none',		0,			'entity',	''},
+					[5]		=	{	8,		0,	 	'Index',	'rword',	'none',		0,			'raw',		''},
+					[6]		=	{	10,		1,	 	'Flag',		'bit',		'set',		1,			'bool',		'[Position, Rotation, Walk]'	},
+					[7]		=	{	12,		0,	 	'Position',	'special',	'use',		1,			'xyz',		''},
+					[8]		=	{	10,		2,	 	'Flag',		'bit',		'set',		2,			'bool',		'[Claimer ID]'	},
+					[9]		=	{	44,		0,	 	'Claimer',	'rdword',	'use',		2,			'player',	''				},
+					[10]	=	{	10,		4,	 	'Flag',		'bit',		'set',		3,			'bool',		'[HP, Status]'	},
+					[11]	=	{	30,		0,	 	'HP %%',	'byte',		'use',		3,			'raw',		''				},
+					[12]	=	{	10,		8,	 	'Flag',		'bit',		'set',		4,			'bool',		'[Name]'		},
 				},
+
+--	Char Update
+	
+	[0x0DF] = T	{			--		Offset	bit		Name		format		logic		flag		decode		Info
+					[1]		=	{	0,		0,	 	'ID',		'byte',		'none',		0,			'raw',		''	},
+					[2]		=	{	1,		0,	 	'Size',		'byte',		'none',		0,			'raw',		''	},
+					[3]		=	{	2,		0,	 	'Sync',		'rword',	'none',		0,			'raw',		''	},
+					[4]		=	{	4,		0,	 	'ID',		'rdword',	'none',		0,			'raw',		''	},
+					[5]		=	{	8,		0,	 	'HP',		'rdword',	'none',		0,			'raw',		''	},
+					[6]		=	{	12,		0,	 	'MP',		'rdword',	'none',		0,			'raw',		''	},
+					[7]		=	{	16,		0,	 	'TP',		'rdword',	'none',		0,			'raw',		''	},
+					[8]		=	{	20,		0,	 	'Index',	'rword',	'none',		0,			'raw',		''	},
+					[9]		=	{	22,		0,	 	'HP %%',	'byte',		'none',		0,			'raw',		''	},
+					[10]	=	{	23,		0,	 	'HP %%',	'byte',		'none',		0,			'raw',		''	},
+				},
+
 	}
 
 return AllRulesIn
--- TickerTape/data/RulesOut.lua
+++ TickerTape/data/RulesOut.lua
@@ -2,10 +2,10 @@ local AllRulesIn = {
 
 --	NPC Update
 	
-	[0x015] = T	{			--		Offset	bit		Name		format		decode		Info
-					[1]		=	{	0,		0,	 	'ID',		'byte',		'raw',		''},
-					[2]		=	{	2,		0,	 	'Sync',		'rword',	'raw',		''},
-					[3]		=	{	4,		0,	 	'Position',	'special',	'xyz',		''},
+	[0x015] = T	{			--		Offset	bit		Name		format		logic		flag		decode		Info
+					[1]		=	{	0,		0,	 	'ID',		'byte',		'none',		0,			'raw',		''},
+					[2]		=	{	2,		0,	 	'Sync',		'rword',	'none',		0,			'raw',		''},
+					[3]		=	{	4,		0,	 	'Position',	'special',	'none',		0,			'xyz',		''},
 				},
 	}
 
